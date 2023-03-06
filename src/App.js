77a77bad42825f3fb2d40d6f3c4e85a73ae7e781
Mansi1236 <radhikey1236@gmail.com>
2023-03-06 12:19:56 +0530

    Added React components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Routes, Link} from "react-router-dom";
+import Home from './pages/Home';
+import Search from './pages/Search';
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
 
 function App() {
-  return (
-    <>
-    </>
+  
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: "https://countries.trevorblades.com",
+});
+
+return (
+
+<ApolloProvider client={client} >
+<div className='App'>
+<Router>
+<Routes>
+<Route path="/" element={<Home/>}/>
+<Route path="search" element={<Search/>} />
+</Routes>
+</Router>
+</div>
+</ApolloProvider>
+
+
+
+  
+
+
   );
 }
 
---
+++ src/pages/Search.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export const Search = () => {
+  return (
+    <div>
+   Search
+
+
+    </div>
+  );
+};
+
+
+export default Search;
