775a094208a114060547835beddd8f24a10c3556
Mansi1236 <radhikey1236@gmail.com>
2023-03-06 13:09:33 +0530

    List of countries API

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,62 @@
 import React from 'react'
+import { useQuery , gql} from '@apollo/client';
+
+
+
+const Query_list_of_countries = gql `
+{
+countries {
+    name
+    capital
+    code
+    phone
+}
+
+
+
+
+}
+
+
+
+
+
+`;
+
+
+
 
 export const Home = () => {
+    const {data, loading, error} = useQuery(Query_list_of_countries);
   return (
-    <div>Home</div>
+    <div className='home'>
+        
+    <h1> List of Countries</h1> 
+       <div className='Countrieslist'>
+{ loading && <h4> Data is loading...</h4>}
+{ error && <h4> {error.message}</h4>}
+{ data && data.countries.map((country , key) => { return <div> 
+<h2> {country.name}</h2>
+<h3> {country.capital}</h3>
+<h3> {country.code}</h3>
+<h3> {country.phone}</h3>
+
+
+</div>
+}
+
+
+)}
+
+       </div>
+        
+        
+        
+        
+        
+        
+        
+        Home</div>
   );
 }
 
