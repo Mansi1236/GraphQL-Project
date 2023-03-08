053d14380b64ba195dda733e9f13db848a7bb0f1
Mansi1236 <radhikey1236@gmail.com>
2023-03-08 23:06:43 +0530

    Formatted the code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,65 +1,42 @@
-import React from 'react'
-import { useQuery , gql} from '@apollo/client';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useQuery, gql } from "@apollo/client";
+import { Link } from "react-router-dom";
 
-
-const Query_list_of_countries = gql `
-{
-countries {
-    name
-    capital
-    code
-    phone
-}
-
-
-
-
-}
-
-
-
-
-
+const Query_list_of_countries = gql`
+  {
+    countries {
+      name
+      capital
+      code
+      phone
+    }
+  }
 `;
 
-
-
-
 export const Home = () => {
-    const {data, loading, error} = useQuery(Query_list_of_countries);
+  const { data, loading, error } = useQuery(Query_list_of_countries);
   return (
-    <div className='home'>
-        
-    <h1> List of Countries</h1> 
-       <div className='Countrieslist'>
-
+    <div className="home">
+      <h1> List of Countries</h1>
+      <div className="Countrieslist">
         <Link to="/search"> Search for Countries</Link>
-{ loading && <h4> Data is loading...</h4>}
-{ error && <h4> {error.message}</h4>}
-{ data && data.countries.map((country , key) => { return <div> 
-<h2> {country.name}</h2>
-<h3> {country.capital}</h3>
-<h3> {country.code}</h3>
-<h3> {country.phone}</h3>
-
-
-</div>
-}
-
-
-)}
-
-       </div>
-        
-        
-        
-        
-        
-        
-        
-        Home</div>
+        {loading && <h4> Data is loading...</h4>}
+        {error && <h4> {error.message}</h4>}
+        {data &&
+          data.countries.map((country, key) => {
+            return (
+              <div>
+                <h2> {country.name}</h2>
+                <h3> {country.capital}</h3>
+                <h3> {country.code}</h3>
+                <h3> {country.phone}</h3>
+              </div>
+            );
+          })}
+        Home
+      </div>
+    </div>
   );
-}
+};
 
 export default Home;
